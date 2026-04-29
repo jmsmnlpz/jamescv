3d9606f78eb41ec494aea70c443a0335f3e1ead0
jmsmnlpz <jasmanlapaz@outlook.ph>
2026-04-29 09:55:42 +0800

    Personal Info Update
    Updated portfolio with personal info, photo and resume

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,68 +6,39 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "James",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Manlapaz",
+  message: "Entrepreneur. Tradesman. Always Learning, Always Building.",
   icons: [
     {
-      image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
-    },
-    {
       image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
+      url: "https://www.facebook.com/james.manlapazzz",
     },
     {
       image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
-    {
-      image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
-    },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
+      url: "https://www.instagram.com/jazmresortandevents/",
     },
   ],
 };
 
 // ABOUT SECTION
-// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
-//a) your Instagram username
-//      i.e:profilePictureLink:"johnDoe123",
-//b) a link to an hosted image
-//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
-//c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
-//d) If you do not want any picture to be displayed, just leave it empty :)
-//      i.e: profilePictureLink: "",
-// For Resume either provide link to your resume or import from "editable-stuff" directory
-//     i.e resume: require("../editable-stuff/resume.pdf"),
-//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
-
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/james.png"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "I'm James Manlapaz — a mechanical engineering graduate from Mapua Institute of Technology and a hands-on business owner based in Cavite, Philippines. I currently run JAZM Garden Hidden Resort as General Manager while also handling administrative operations for a medical equipment repair and maintenance business. I'm driven by constant learning, technical precision, and a deep commitment to building things that last.",
+  resume: require("../editable-stuff/resume.pdf"),
 };
 
 // PROJECTS SECTION
-// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
-//      i.e: reposLength: 0,
-// If you want to display specfic projects, add the repository names,
-//      i.e ["repository-1", "repo-2"]
 const repos = {
-  show: true,
+  show: false,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
-  reposLength: 4,
+  gitHubUsername: "",
+  reposLength: 0,
   specificRepos: [],
 };
 
@@ -75,24 +46,12 @@ const repos = {
 const leadership = {
   show: false,
   heading: "Leadership",
-  message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
-  images: [
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-  ],
+  message: "",
+  images: [],
   imageSize: {
-    width:"615",
-    height:"450"
-  }
+    width: "615",
+    height: "450",
+  },
 };
 
 // SKILLS SECTION
@@ -100,24 +59,28 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "Welding (TIG, MIG, Fiber Laser, Stud, Spot)", value: 90 },
+    { name: "Metal Fabrication & Assembly", value: 90 },
+    { name: "Swimming Pool Maintenance", value: 85 },
+    { name: "Resort Facility Maintenance", value: 85 },
+    { name: "Bookkeeping & Budget Tracking", value: 75 },
+    { name: "Government Compliance", value: 75 },
+    { name: "MS Office (Excel, Word, PowerPoint)", value: 80 },
+    { name: "AutoCAD", value: 70 },
+    { name: "Python & VS Code", value: 60 },
+    { name: "Woodworks & Masonry", value: 60 },
   ],
   softSkills: [
-    { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
+    { name: "Working Under Pressure", value: 90 },
+    { name: "Technical Analysis", value: 90 },
+    { name: "Attention to Detail", value: 90 },
+    { name: "Organization", value: 90 },
+    { name: "Goal-Oriented", value: 85 },
     { name: "Adaptability", value: 85 },
-    { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
+    { name: "Problem Solving", value: 85 },
+    { name: "Creativity", value: 80 },
+    { name: "Leadership", value: 60 },
+    { name: "Communication", value: 45 },
   ],
 };
 
@@ -126,30 +89,31 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "Feel free to reach out for business inquiries, collaborations, or just to say hello. You can email me at",
+  email: "jasmanlapaz@outlook.ph",
 };
 
+// EXPERIENCE SECTION
 const experiences = {
   show: false,
-  heading: "Experiences",
+  heading: "Experience",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      role: "Business Owner / General Manager — JAZM Garden Hidden Resort",
+      companylogo: require("../assets/img/dell.png"),
+      date: "May 2022 – Present",
     },
     {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      role: "Administrative Manager — Manlapas Medical Equipment Services",
+      companylogo: require("../assets/img/boeing.png"),
+      date: "August 2021 – Present",
     },
-  ]
-}
-
-// Blog SECTION
-// const blog = {
-//   show: false,
-// };
+    {
+      role: "Production Intern — Ninomiya Manufacturing Corporation",
+      companylogo: require("../assets/img/boeing.png"),
+      date: "November 2019 – November 2020",
+    },
+  ],
+};
 
 export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
